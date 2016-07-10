a7f2c0882eb9d39b6594f33d840e5d204622c4b8
Jane Wong <jaynellew@gmail.com>
2016-07-10 16:00:38 -0700

    Set up reducer and React Store

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 // The 'App' component is the root route
 // The root component serves as the template, rendering the markup that
