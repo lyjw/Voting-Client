f822578da711ef6539fa5353944b0b12eff7de27
Jane Wong <jaynellew@gmail.com>
2016-07-10 15:39:34 -0700

    Create Tally component and refactor Results component

---
+++ src/components/Tally.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  getVotes: function(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  },
+  render: function() {
+    return <div className="tally">
+              {this.props.pair.map(entry =>
+                <div key={entry} className="entry">
+                  <h1>{entry}</h1>
+                  <div className="voteCount">
+                    {this.getVotes(entry)}
+                  </div>
+                </div>
+              )}
+            </div>;
+  }
+});
