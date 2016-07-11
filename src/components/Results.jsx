aee5e7bae0e30e31b5b9ef76d2742c9d74638499
Jane Wong <jaynellew@gmail.com>
2016-07-11 16:04:17 -0700

    - Connect Results and Voting components to Redux store - Refactor App component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Tally from './Tally';
 
-export default React.createClass({
+export const Results = React.createClass({
   mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
@@ -23,3 +24,13 @@ export default React.createClass({
       </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  }
+}
+
+expect const ResultsContainer = connect(mapStateToProps)(Results);
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {conenect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
 
-export default React.createClass({
+export const Voting = React.createClass({
   mixins: [PureRenderMixin],
-  getPair: function() {
-    return this.props.pair || [];
-  },
-  isDisabled: function() {
-    return !!this.props.hasVoted;
-  },
-  hasVotedFor: function(entry) {
-    return this.props.hasVoted === entry;
-  },
   render: function() {
     return <div>
       {this.props.winner ?
         // ref grabs the corresponding DOM node
         <Winner ref="winner" winner={this.props.winner} /> :
-        <Vote {...this.props} />
-      }
-    </div>
+        <Vote {...this.props} />}
+    </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+// connect returns a connected version of Voting
+// The module now exports two components - the pure component ('Voting') and the connected component ('VotingContainer')
+export const VotingContainer = connect(mapStateToProps)(Voting);
